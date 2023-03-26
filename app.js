8affc5b5b2c33d91877eacdb9f181c7dda5dc65f
mehedi hasan <mdmehedihasan1437@gmail.com>
2023-03-26 12:20:17 +0600

    search result done

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('files are connented');
+const loadPhones = async(searchText) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+   const res = await fetch(url);
+   const data = await res.json();
+   displayPhones(data.data);
+}
+
+
+const displayPhones = phones =>{
+   console.log(phones);
+
+   const phoneContainer = document.getElementById('phone-container');
+   phoneContainer.textContent = '';
+   phones.forEach(phone => {
+     console.log(phone);
+     const phoneDiv = document.createElement('div');
+     phoneDiv.classList.add('col');
+     phoneDiv.innerHTML = `
+     <div class="card p-4">
+     <img src="${phone.image}" class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">Name : ${phone.phone_name}</h5>
+       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+     </div>
+   </div>
+     
+     
+     
+     `
+     phoneContainer.appendChild(phoneDiv);
+   })
+}
+
+
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value ; 
+    loadPhones(searchText);
+})
+
+loadPhones();
